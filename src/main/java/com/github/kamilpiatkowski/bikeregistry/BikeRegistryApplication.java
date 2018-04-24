40631bfe93cf51af99fece69c0a973c21771f226
Kamil <kamilpiatkowski@gmail.com>
2018-04-24 18:21:12 +0200

    Initial commit

---
+++ src/main/java/com/github/kamilpiatkowski/bikeregistry/BikeRegistryApplication.java
@@ -0,0 +1,12 @@
+package com.github.kamilpiatkowski.bikeregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BikeRegistryApplication.class, args);
+	}
+}
