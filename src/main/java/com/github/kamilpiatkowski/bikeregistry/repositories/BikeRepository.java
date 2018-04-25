8a24404b8a258b87bf645beab3377eea1ed00ff2
Kamil <kamilpiatkowski@gmail.com>
2018-04-25 22:59:22 +0200

    Add persistence layer

--- src/main/java/com/github/kamilpiatkowski/bikeregistry/repositories/BikeRepository.java
+++ src/main/java/com/github/kamilpiatkowski/bikeregistry/repositories/BikeRepository.java
@@ -2,7 +2,9 @@ package com.github.kamilpiatkowski.bikeregistry.repositories;
 
 import com.github.kamilpiatkowski.bikeregistry.models.Bike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BikeRepository extends JpaRepository<Bike, Long> {
 
 }
